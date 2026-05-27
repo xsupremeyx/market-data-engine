794f3368dab58a65fe41126c37e4747f23ee89ac
Yash <86472828+xsupremeyx@users.noreply.github.com>
2026-05-27 18:47:19 +0000

    feat: Add updateTimeframe util for websockets.

---
+++ src/utils/updateTimeframe.js
@@ -0,0 +1,61 @@
+const { alignTimestamp } = require('./alignTimestamp');
+
+function updateTimeframe(
+    timeframeCandles,
+    newCandle,
+    timeframeMinutes
+){
+
+    const bucketTime = alignTimestamp(
+        newCandle.time,
+        timeframeMinutes
+    );
+
+    const lastCandle =
+        timeframeCandles[
+            timeframeCandles.length - 1
+        ];
+
+    if(
+        !lastCandle ||
+        lastCandle.time !== bucketTime
+    ){
+
+        timeframeCandles.push({
+
+            time: bucketTime,
+
+            open: newCandle.open,
+
+            high: newCandle.high,
+
+            low: newCandle.low,
+
+            close: newCandle.close,
+
+            volume: newCandle.volume,
+        });
+
+    } else {
+
+        lastCandle.high = Math.max(
+            lastCandle.high,
+            newCandle.high
+        );
+
+        lastCandle.low = Math.min(
+            lastCandle.low,
+            newCandle.low
+        );
+
+        lastCandle.close =
+            newCandle.close;
+
+        lastCandle.volume +=
+            newCandle.volume;
+    }
+}
+
+module.exports = {
+    updateTimeframe,
+};
